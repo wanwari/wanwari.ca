4577ef565fb78a2eabb21e7b88a103171e2c0dd3
Wiesa Anwari <wiesa@wanwari.ca>
2022-07-21 21:27:15 -0400

    setup: install & setup react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Portfolio from "./pages/Portfolio";
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/portfolio" element={<Portfolio />} />
+			<Route path="/contact" element={<Contact />} />
+			<Route path="*" element={<NotFound />} />
+		</Routes>
+	);
 };
 
 export default App;
