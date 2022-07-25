02debab2816045fc2a9c45a67651ea8287c60845
Wiesa Anwari <wiesa@wanwari.ca>
2022-07-25 14:28:09 -0400

    fix: re-add default tailwind colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
-		colors: {
-			bgOrange: "#FCB07E",
+		extend: {
+			colors: {
+				bgOrange: "#FCB07E",
+			},
 		},
 	},
 	plugins: [],
