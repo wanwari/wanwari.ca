dbc5d4dfc959072ed2e31b8c3b1e9c0fb44e10bb
Wiesa Anwari <wiesa@wanwari.ca>
2022-07-27 18:54:44 -0400

    feat: create welcome page conten using react-spring

---
+++ src/components/animated/DropText.tsx
@@ -0,0 +1,18 @@
+import { useSpring, animated } from "react-spring";
+
+const DropText = () => {
+	const animationProps = useSpring({
+		from: { opacity: 0, marginTop: -300 },
+		to: { opacity: 1, marginTop: -175 },
+	});
+
+	return (
+		<div>
+			<animated.div style={animationProps} className="text-8xl">
+				Hi, I'm <span className="font-bold">Wiesa</span>
+			</animated.div>
+		</div>
+	);
+};
+
+export default DropText;
---
+++ src/components/animated/TypeText.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState, useCallback } from "react";
+
+const TypeText = () => {
+	const [textBar, setTextBar] = useState(" ");
+	const text = "I'm a passionate full-stack developer :)";
+	const delay = 750;
+
+	const typeText = useCallback((text: string) => {
+		for (let pos = 0; pos <= text.length; pos++) {
+			setTimeout(() => {
+				setTextBar(text.substring(0, pos));
+			}, pos * 65);
+		}
+	}, []);
+
+	useEffect(() => {
+		setTimeout(() => {
+			typeText(text);
+		}, delay);
+	}, [typeText, text, delay]);
+
+	return <div className="text-3xl">{textBar}</div>;
+};
+
+export default TypeText;
