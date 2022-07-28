a661ad99265ed34450df51db499388557eb61cef
Wiesa Anwari <wiesa@wanwari.ca>
2022-07-28 15:07:16 -0400

    feat: animate cards

--- src/components/animated/Card.tsx
+++ src/components/animated/Card.tsx
@@ -1,6 +1,8 @@
 import { useSpring, animated } from "react-spring";
+import logo from "../../img/logo.jpg";
 
 interface CardProps {
+	delay: number;
 	title: string;
 	about: string;
 	tags: string;
@@ -8,40 +10,48 @@ interface CardProps {
 	demoUrl?: string;
 }
 
-const Card = ({ title, about, tags, codeUrl, demoUrl }: CardProps) => {
+const Card = ({ delay, title, about, tags, codeUrl, demoUrl }: CardProps) => {
+	const cardAnimation = useSpring({
+		from: { opacity: 0, marginTop: -300 },
+		to: { opacity: 1, marginTop: 0 },
+		delay: delay,
+	});
+
 	return (
-		<div className="flex flex-col justify-end  border rounded-xl h-fit p-4 m-4 mb-8 bg-white shadow-xl">
-			<div className="flex justify-center">
-				<img
-					src="https://github.com/creativetimofficial/soft-ui-dashboard-tailwind/blob/main/build/assets/img/team-2.jpg?raw=true"
-					className="w-1/5 rounded-full -mt-10"
-					alt="logo"
-				/>
-			</div>
-			<div className="flex justify-center flex-col text-center mb-4">
-				<h1 className="font-bold text-3xl mt-6">{title}</h1>
-				<p className="text-slate-500 uppercase">{tags}</p>
-			</div>
-			<div className="flex justify-center p-2 border-t border-slate-300">
-				<p>{about}</p>
-			</div>
-			<div className="flex justify-center">
-				<a
-					href={codeUrl}
-					className="bg-green-400 p-3 rounded-xl hover:text-white hover:shadow-lg m-2"
-				>
-					View Code
-				</a>
-				{demoUrl && (
+		<animated.div style={cardAnimation}>
+			<div className="flex flex-col justify-end  border rounded-xl h-fit p-4 m-4 mb-8 bg-white shadow-xl">
+				<div className="flex justify-center">
+					<img
+						src={logo}
+						className="w-1/5 rounded-full -mt-10"
+						alt="logo"
+					/>
+				</div>
+				<div className="flex justify-center flex-col text-center mb-4">
+					<h1 className="font-bold text-3xl mt-6">{title}</h1>
+					<p className="text-slate-500 uppercase">{tags}</p>
+				</div>
+				<div className="flex justify-center p-2 border-t border-slate-300">
+					<p>{about}</p>
+				</div>
+				<div className="flex justify-center">
 					<a
-						href={demoUrl}
+						href={codeUrl}
 						className="bg-green-400 p-3 rounded-xl hover:text-white hover:shadow-lg m-2"
 					>
-						Visit Project
+						View Code
 					</a>
-				)}
+					{demoUrl && (
+						<a
+							href={demoUrl}
+							className="bg-green-400 p-3 rounded-xl hover:text-white hover:shadow-lg m-2"
+						>
+							Visit Project
+						</a>
+					)}
+				</div>
 			</div>
-		</div>
+		</animated.div>
 	);
 };
 
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -4,11 +4,12 @@ import Header from "../components/Header";
 
 const Portfolio = () => {
 	return (
-		<div className="bg-bgGreen flex flex-col justify-between">
+		<div className="bg-bgGreen h-screen flex flex-col justify-between overflow-auto">
 			<Header active={"portfolio"} />
 
 			<div className="grid grid-cols-3 m-12 pt-4 justify-items-center">
 				<Card
+					delay={600}
 					title={"Title"}
 					about={
 						"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat nisl ex, vel tincidunt mi ullamcorper nec. Suspendisse scelerisque eu nulla a lacinia. Nam interdum est velit, id gravida tortor finibus ornare. Integer sit amet ligula nec quam tempus lacinia. Donec scelerisque arcu nec sapien rhoncus pharetra."
@@ -18,28 +19,34 @@ const Portfolio = () => {
 					demoUrl={"Title"}
 				/>
 				<Card
+					delay={400}
 					title={"Title"}
 					about={
 						"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat nisl ex, vel tincidunt mi ullamcorper nec. Suspendisse scelerisque eu nulla a lacinia. Nam interdum est velit, id gravida tortor finibus ornare. Integer sit amet ligula nec quam tempus lacinia. Donec scelerisque arcu nec sapien rhoncus pharetra."
 					}
 					tags={"JavaScript, TypeScript, ReactJS, TailwindCSS, HTML"}
 					codeUrl={"Title"}
+					demoUrl={"Title"}
 				/>
 				<Card
+					delay={200}
 					title={"Title"}
 					about={
 						"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat nisl ex, vel tincidunt mi ullamcorper nec. Suspendisse scelerisque eu nulla a lacinia. Nam interdum est velit, id gravida tortor finibus ornare. Integer sit amet ligula nec quam tempus lacinia. Donec scelerisque arcu nec sapien rhoncus pharetra."
 					}
 					tags={"JavaScript, TypeScript, ReactJS, TailwindCSS, HTML"}
 					codeUrl={"Title"}
+					demoUrl={"Title"}
 				/>
 				<Card
+					delay={0}
 					title={"Title"}
 					about={
 						"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat nisl ex, vel tincidunt mi ullamcorper nec. Suspendisse scelerisque eu nulla a lacinia. Nam interdum est velit, id gravida tortor finibus ornare. Integer sit amet ligula nec quam tempus lacinia. Donec scelerisque arcu nec sapien rhoncus pharetra."
 					}
 					tags={"JavaScript, TypeScript, ReactJS, TailwindCSS, HTML"}
 					codeUrl={"Title"}
+					demoUrl={"Title"}
 				/>
 			</div>
 			<Footer />
