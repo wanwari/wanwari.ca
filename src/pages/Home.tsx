b5d498f58b6ca265ca69093a79d896c24e5a56ad
Wiesa Anwari <wiesa@wanwari.ca>
2022-07-27 21:21:00 -0400

    refactor: restrucutre Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,17 @@
+import Header from "../components/Header";
+import DropText from "../components/animated/DropText";
+import TypeText from "../components/animated/TypeText";
 import ArrowBtn from "../components/ArrowBtn";
 import Footer from "../components/Footer";
-import Header from "../components/Header";
-import Welcome from "../components/Welcome";
 
 const Home = () => {
 	return (
+		//<div className="h-screen flex flex-col justify-between bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
 		<div className="h-screen bg-bgOrange flex flex-col justify-between">
 			<Header active={"home"} />
-			<div>
-				<Welcome />
+			<div className="text-center">
+				<DropText />
+				<TypeText />
 				<div className="flex flex-row justify-center mt-4">
 					<ArrowBtn direction={"up"} />
 					<ArrowBtn direction={"down"} />
