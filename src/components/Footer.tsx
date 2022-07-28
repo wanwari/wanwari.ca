f2447c1e6e35820fe933081fd1d4961aa4a8a5b5
Wiesa Anwari <wiesa@wanwari.ca>
2022-07-27 22:54:39 -0400

    feat: animate footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,23 @@
 import { FaHeart } from "react-icons/fa";
 import { IconContext } from "react-icons";
+import { useSpring, animated } from "react-spring";
 
 const Footer = () => {
+	const animationProps = useSpring({
+		loop: { reverse: true },
+		from: { marginTop: -15 },
+		to: { marginTop: 0 },
+		delay: 100,
+	});
 	return (
 		<div className="flex flex-row justify-end p-6">
-			<div>
-				<IconContext.Provider value={{ color: "#fff" }}>
-					<span className="pr-2">Developed with</span>
+			<IconContext.Provider value={{ color: "#fff" }}>
+				<span className="pr-2">Developed with</span>
+				<animated.div style={animationProps}>
 					<FaHeart className="inline" />
-					<span className="pl-2">by Wiesa Anwari</span>
-				</IconContext.Provider>
-			</div>
+				</animated.div>
+				<span className="pl-2">by Wiesa Anwari</span>
+			</IconContext.Provider>
 		</div>
 	);
 };
